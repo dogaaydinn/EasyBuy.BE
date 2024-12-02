6eac311b7da6b41e222c32fc8c515384bb88196f
doga <dogaa882@gmail.com>
2024-12-02 21:22:38 +0300

    Add shadow properties for entity tracking

---
+++ Infrastructure/EasyBuy.Persistence/Constants/CommonShadowProperties.cs
@@ -0,0 +1,8 @@
+namespace EasyBuy.Persistence.Constants;
+
+internal class CommonShadowProperties
+{
+    public const string CreatedDate = nameof(CreatedDate);
+    public const string ModifiedDate = nameof(ModifiedDate);
+    public const string DeletedDate = nameof(DeletedDate);
+}
